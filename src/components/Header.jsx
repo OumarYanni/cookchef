5c297e2d8bb7cd15adb22cae8f2f0b5a0d16513a
Oumar Yanni <oumaryanni@gmail.com>
2024-09-19 22:36:52 +0000

    Setting up the Header and Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
+import styles from "./Header.module.scss";
+import cookchef from "../assets/images/cookchef.png";
+
 function Header() {
-  return <h1 className="b1">Header</h1>;
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <span className="material-symbols-outlined mr-15">menu</span>
+
+      <div className="flex-fill">
+        <img src={cookchef} alt="logo cookchef" />
+      </div>
+
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <span className="material-symbols-outlined mr-5">
+            shopping_basket
+          </span>
+
+          <span>panier</span>
+        </button>
+
+        <button className="btn btn-primary">connexion</button>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
