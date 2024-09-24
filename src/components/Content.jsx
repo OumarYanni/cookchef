6f5d9b7453a47b0386e60c02e0d1b86340751817
Oumar Yanni <oumaryanni@gmail.com>
2024-09-24 20:30:44 +0000

    Add a search bar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,42 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
 import { data } from "../data/recipes";
+import { useState } from "react";
 
 function Content() {
   const recipes = data;
+
+  const [filter, setFilter] = useState("");
+
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
 
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
 
-      <div className={`card p-20 ${styles.contentCard} br`}>
+      <div className={`card p-20 d-flex flex-column  ${styles.contentCard}`}>
+        <div
+          className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+        >
+          <span className="material-symbols-outlined mr-15">search</span>
+
+          <input
+            onInput={handleInput}
+            className="flex-fill"
+            type="text"
+            placeholder="Rechercher"
+          />
+        </div>
+
         <div className={styles.grid}>
-          {recipes.map((r) => (
-            <Recipe key={r._id} title={r.title} image={r.image} />
-          ))}
+          {recipes
+            .filter((r) => r.title.toLowerCase().startsWith(filter))
+            .map((r) => (
+              <Recipe key={r._id} title={r.title} image={r.image} />
+            ))}
         </div>
       </div>
     </div>
