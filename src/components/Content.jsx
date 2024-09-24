4b7e140dac02eb3f90f958552a07d415bb497000
Oumar Yanni <oumaryanni@gmail.com>
2024-09-24 17:34:08 +0000

    Make the application responsive

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -9,7 +9,7 @@ function Content() {
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
 
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card p-20 ${styles.contentCard} br`}>
         <div className={styles.grid}>
           {recipes.map((r) => (
             <Recipe key={r._id} title={r.title} image={r.image} />
